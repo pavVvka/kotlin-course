104f90863b8400e57c7117958a1e368ee5f5af6c
pavvv <pavvvka@gmail.com>
2024-09-19 14:16:38 +0300

    Lesson 4 - HomeWork added

---
+++ src/main/kotlin/org/pavvvka/kotlincourse/lesson4/Lesson4.kt
@@ -0,0 +1,47 @@
+package org.pavvvka.org.pavvvka.kotlincourse.lesson4
+
+class Lesson4 {
+
+    val int1: Int = 42
+    val long1: Long = 98765432123456789L
+    val float1: Float = 23.45f
+    val double1: Double = 0.123456789
+    val string1: String = "Kotlin & Java"
+    val boolean1: Boolean = false
+    val char1: Char = 'c'
+    val int2: Int = 500
+    val long2: Long = 4294967296L
+    val float2: Float = 18.0f
+    val double2: Double = -0.001
+    val string2: String = "OpenAI"
+    val string3: String = "true"
+    val char2: Char = '9'
+    val int3: Int = 2048
+    val long3: Long = 10000000000L
+    val float3: Float = 5.75f
+    val double3: Double = 1.414
+    val string4: String = "Artificial Intelligence"
+    val boolean2: Boolean = false
+    val char3: Char = '@'
+    val int4: Int = 1024
+    val long4: Long = 1234567890123L
+    val float4: Float = 10.01f
+    val double4: Double = -273.15
+    val string5: String = "SpaceX"
+    val boolean3: Boolean = true
+    val char4: String = "🤯"
+    // val needSpecify: ? = '65535'
+    val stringToSpecify: String = "65535"
+    val charToSpecify: Char = 65535.toChar()
+    val long5: Long = 72057594037927935L
+    val float5: Float = 2.71828f
+    val double5: Double = 101.0101
+    val string6: String = "Quantum Computing"
+    val boolean4: Boolean = false
+    val char6: Char = 'x'
+    val int5: Int = 314
+    val long6: Long = 123456789123456789L
+    val float6: Float = 6.626f
+    val double6: Double = 0.007
+    val string7: String = "Android Studio"
+}
