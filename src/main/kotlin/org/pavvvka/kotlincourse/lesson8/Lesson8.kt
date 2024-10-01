2ab376f8ea649b16a0af48570871f11dff564afb
pavvv <pavvvka@gmail.com>
2024-10-01 16:20:24 +0300

    added file in gitignore

---
+++ src/main/kotlin/org/pavvvka/kotlincourse/lesson8/Lesson8.kt
@@ -0,0 +1,266 @@
+package org.pavvvka.kotlincourse.lesson8
+
+fun main() {
+    println(phraseChecker("Это невозможно выполнить за один день"))
+    println(phraseChecker("Я не уверен в успехе этого проекта"))
+    println(phraseChecker("Произошла катастрофа на сервере"))
+    println(phraseChecker("Этот код работает без проблем"))
+    println(phraseChecker("Удача"))
+
+    /*
+    Задание 1: Извлечение Даты из Строки Лога.
+    Используй indexOf или split для получения правой части сообщения.
+    Описание: У вас есть строка лога вида "Пользователь вошел в систему -> 2021-12-01 09:48:23".
+    Извлеките отдельно дату и время из этой строки и сразу распечатай их по очереди.
+     */
+    val log = "Пользователь вошел в систему -> 2021-12-01 09:48:23"
+    println("\n>>> Задание 1: Извлечение Даты из Строки Лога.\n${log}")
+    println(getDataFromLog(log))
+
+    /*
+    Задание 2: Маскирование Личных Данных
+    Описание: Дана строка с номером кредитной карты "4539 1488 0343 6467".
+    Замаскируйте все цифры, кроме последних четырех, символами "*".
+     */
+    val cardNumber = "4539 1488 0343 6467"
+    println("\n>>> Задание 2: Маскирование Личных Данных.\n${cardNumber}")
+    maskNumber(cardNumber)
+
+    /*
+    Задание 3: Форматирование Адреса Электронной Почты. Используй replace
+    Описание: У вас есть электронный адрес "username@example.com".
+    Преобразуйте его в строку "username [at] example [dot] com".
+     */
+    val emailSample = "username@example.com"
+    println("\n\n>>> Задание 3: Форматирование Адреса Электронной Почты: $emailSample")
+    emailToString(emailSample)
+
+    /*
+    Задание 4: Извлечение Имени Файла из Пути
+    Описание: Дан путь к файлу "C:/Пользователи/Документы/report.txt".
+    Извлеките название файла с расширением.
+     */
+    val path = "C:/Пользователи/Документы/report.txt"
+    println("\n\n>>> Задание 4: Извлечение Имени Файла из Пути: $path")
+    getFileName(path)
+
+    /*
+    Задание 5: Создание Аббревиатуры из Фразы.
+    - Используй split с набором символов для разделения.
+    - Используй for для перебора слов.
+    - Используй var переменную для накопления первых букв.
+    Описание: У вас есть фраза, например "Объектно-ориентированное программирование".
+    Создайте аббревиатуру из начальных букв слов (например, "ООП").
+     */
+    val phrase = "Объектно-ориентированное программирование"
+    println("\n\n>>> Задание 5: Создание Аббревиатуры из Фразы: $phrase")
+    abbreviatePhrase(phrase)
+
+    /*  --- 6 ---
+    Написать метод, который преобразует строку из нескольких слов в строку,
+    где каждое слово начинается с заглавной буквы а все остальные - строчные.
+    (можно использовать такой же подход как в задании 5,
+    но накапливать не первые буквы а целиком слова,
+    составленные из первой буквы с uppercase и оставшейся части слова)
+     */
+    val text = "ОбъектНо-ориентирОванное, программирование"
+    println("\n\n>>> Задание ---6---: строку из нескольких слов в строку: $text")
+    printWordsFromPhrase(phrase)
+
+    /* --- 7 ---
+    Написать шифратор/дешифратор для строки.
+    Шифровка производится путём замены двух соседних букв между собой:
+    Kotlin шифруется в oKltni.
+    Дешифровка выполняется аналогично.
+    Если длина строки - нечётная,
+    в конец добавляется символ пробела до начала шифрования.
+    Таким образом все шифрованные сообщения будут с чётной длинной.
+    Должно получиться два публичных метода:
+    encrypt() и decrypt() которые принимают и возвращают строку.
+     */
+    val txt = "ОбъектНо-"
+    println("\n\n>>> Задание ---7---: шифратор/дешифратор для строки: $txt")
+    println(encrypt(txt))
+    val sample = encrypt(txt)
+    decrypt(sample)
+
+    /* --- 8 ---
+    Вывести таблицу умножения в консоль так, чтобы были ровные строки и столбцы.
+    В заголовках столбцов и строк - числа для перемножения.
+    В перекрестии - результат умножения.
+    Каждый столбец должен быть выровнен по правому краю.
+    Для выравнивания использовать функции форматирования строк с шаблоном (%s)
+     */
+    println("\n\n>>> Задание ---8---: Вывести таблицу умножения")
+    val size = 10
+    matrix(size)
+}
+
+fun matrix(size: Int) {
+    val indent = (size * size).toString().length // ширина поля = кол. знаков у максимального числа
+    val columnWidth = indent + 1 // поле плюс отступ
+
+    // строка - заголовок
+    var firstString = " ".repeat(indent) // первый отступ
+    for (i in 1..size) {
+        firstString += "%${columnWidth}d".format(i)
+    }
+    println(firstString)
+
+    // строки: заголовок + результат
+    for (y in 1..size) {
+        print("%${indent}d".format(y)) // номер строки без отступа
+        for (x in 1..size) {
+            print("%${columnWidth}d".format(x * y))
+        }
+        println()
+    }
+}
+
+/**
+ * Печатает строку: Меняет местами каждые два символа заданной строки.
+ */
+fun decrypt(txt: String) {
+    var result = ""
+    for (i in 2..txt.length step 2) {
+        result += txt.substring(i - 2, i).reversed()
+//        result += txt.substring(i - 1, i) + txt.substring(i - 2, i -1) // вариант
+    }
+    println(">>${result.trim()}")
+}
+
+/**
+ * Добавляет пробел к строке, если нечетное кол. символов.
+ * Меняет местами каждые два символа заданной строки.
+ *
+ * @return получившуюся строку
+ */
+fun encrypt(txt: String): String {
+    var string = txt
+    if (txt.length % 2 != 0) {
+        string += " "
+    }
+    println(string)
+
+    var resultString = ""
+    var index = 0
+    while (string.length != resultString.length) {
+        resultString += string.substring(index, index + 2).reversed()
+        index += 2
+    }
+    println(resultString)
+    return resultString
+}
+
+/**
+ * Делит строку на подстроки по заданным символам.
+ * Выводит строку из подстрок, делая первую букву каждой заглавной,
+ * а оставшуюся часть подстроки - маленькими.
+ */
+fun printWordsFromPhrase(text: String) {
+    val splittedPhrase = text.split("-", " ", ",")
+    var result = ""
+    for (i in splittedPhrase) {
+        result += "${i.first().uppercase()}${i.substring(1).lowercase()} "
+    }
+    println(result)
+}
+
+/**
+ * Печатает первый символ элемента строки в верхнем регистре.
+ */
+fun abbreviatePhrase(phrase: String) {
+    val splittedPhrase = phrase.split("-", " ")
+    var result = ""
+    for (i in splittedPhrase) {
+//        print(i.first().uppercase())            // вариант 1
+//        result += i.first().uppercase()         // вариант 2
+        result += i.substring(0, 1).uppercase()   // вариант 3
+    }
+    println(result)
+}
+
+/**
+ * извлекает из строки строку после последнего вхождения "/"
+ */
+fun getFileName(text: String) {
+    println("Вариант 1")
+    println(text.split("/").last())
+
+    println("Вариант 2")
+    println(text.substring(text.lastIndexOf("/") + 1))
+
+}
+
+/**
+ * Меняет "@" "." в строке на " \[at] " " \[dot] "
+ */
+fun emailToString(email: String) {
+    println("Вариант 1")
+    val eName = email.substring(0, email.indexOf('@'))
+    val eDomen = email.substring(email.indexOf('@'), email.indexOf('.')).trim('@')
+    val eTopDomen = email.substring(email.indexOf('.')).trim('.')
+    println("$eName [at] $eDomen [dot] $eTopDomen")
+
+    println("Вариант 2")
+    val em = email
+        .replace("@", " [at] ")
+        .replace(".", " [dot] ")
+    println(em)
+}
+
+/**
+ * печатает строку звезд и последний элемент аргумент-массива, разбитого по пробеллам
+ */
+fun maskNumber(number: String) {
+//    val stars = "**** "
+//    for (i in 0..2) {
+//        print(stars)
+//    }
+    repeat(3) { print("**** ") }
+    println(number.split(" ")[3])
+}
+
+/**
+ * Распечатывает дату и время по очереди из Строки Лога.
+ *     Использует indexOf или split для получения правой части сообщения.
+ */
+fun getDataFromLog(log: String): String {
+    // вариант1
+//    return log.substring(32).substring(0, 11) +
+//            "\n" +
+//            log.substring(32).substring(11)
+
+    // вариант2
+    val data = log.substring(log.indexOf("> ") + 2)
+    return data.split(" ")[0] + "\n" + data.split(" ")[1]
+}
+
+/**
+ * анализирует входящие фразы и применяет к ним различные преобразования
+ *
+ * - "невозможно": "невозможно" на "совершенно точно возможно, просто требует времени"
+ * - "Я не уверен":  Добавьте в конец фразы ", но моя интуиция говорит об обратном".
+ * - "катастрофа": Замените "катастрофа" на "интересное событие".
+ * - "без проблем": Замените "без проблем" на "с парой интересных вызовов на пути".
+ * - только одно слово: Добавьте перед словом "Иногда," и после слова ", но не всегда".
+ */
+fun phraseChecker(phrase: String): String {
+    return when {
+        phrase.contains("невозможно") -> phrase.replace(
+            "невозможно",
+            "совершенно точно возможно, просто требует времени"
+        )
+
+        phrase.contains("Я не уверен") -> "$phrase, но моя интуиция говорит об обратном"
+        phrase.contains("катастрофа") ->
+            phrase.replace("катастрофа", "интересное событие").replace("шла", "шло")
+
+        phrase.contains("без проблем") -> phrase.replace("без проблем", "с парой интересных вызовов на пути")
+        !phrase.contains(" ") -> "Иногда ${phrase.lowercase()}, но не всегда"
+        else -> phrase
+    }
+}
+
+class Lesson8 {
+}
---
+++ src/main/kotlin/org/pavvvka/kotlincourse/lesson8/Lesson8Lection.kt
@@ -0,0 +1,152 @@
+package org.pavvvka.kotlincourse.lesson8
+
+
+fun main() {
+    val simpleStrin = "Это пустая срока"
+    val firstName = "Иван"
+    val lastName = "Иванов"
+    val fullNmae = "$firstName $lastName"
+
+    val age = 30
+    val greeting = "Привет! Меня зовут $fullNmae, и мне $age лет."
+    println(greeting)
+
+    println("\n ==== создаём экземпляр объекта")
+    val person = Person("Алексей", 25)
+    val introduction = "Меня зовут ${person.name}, и мне ${person.age} лет."
+    println(introduction)
+
+    println("\n ==== в шаблоне")
+    val details = "Здесь находятся ${getDetails()}"
+    println(details)
+
+    println("\n ==== в шаблоне2")
+    val x = 10
+    val y = 20
+    val resultString = "Результат сложения Х и У равен ${x + y}"
+    println(resultString)
+
+    ///  Методы Строки
+    println("\n ==== .substring()")
+    val originalString = "Kotlin is fun"
+    val subString1 = originalString.substring(7)
+    val subString2 = originalString.substring(3, 6) // (вкл, не вкл)
+    val replacedString = originalString.replace("n", "@")
+    val words = originalString.split(" ")
+    val words2 = originalString.split("is ")
+//                      "Kotlin is fun"
+// >words               :[Kotlin, is, fun]
+// >words2  " is "      :[Kotlin, fun]
+// >words2  "is "       :[Kotlin , fun]
+
+    println(">subString1      :$subString1")
+    println(">subString2      :$subString2")
+    println(">replacedString  :$replacedString")
+    println(">words           :$words")
+    println(">words2          :$words2")
+
+    // Методы для преобразования строк (действия)
+    val length = "Hello".length
+    println(">length          :$length")
+    val upper = "hello".uppercase()
+    val lower = "HELLO".lowercase()
+    println(">upper           :$upper")
+    val trimmed1 = "    hellow  .".trim()                     //hellow  .// пробелы остаются
+    val trimmed2 = "    hellow   ".trim()                     //hellow//
+    val trimmed3 = "    hello3  .".trim('h', '.', ' ') //ello3//
+    println(">trim1           :$trimmed1")
+    println(">trim2           :$trimmed2")
+    println(">trim3           :$trimmed3")
+
+    // Методы проверки
+    val starts = "Kotlin".startsWith("Kot")
+    val ends = "Kotlin".startsWith("in")
+    println(">starts          :$starts")   // :true
+    println(">ends            :$ends")     // :false
+    val contains = "Hello".contains("ell")
+    println(">contains        :$contains") // :true
+    val e: String? = null
+    val empty = e.isNullOrEmpty()
+    val blank = "  ".isNullOrBlank()
+    println(">empty           :$empty") // :true
+    println(">blank           :$blank") // :true
+
+    // индексы с нуля
+    val repeat = "ab".repeat(3)
+    val letter = originalString[5]
+    val indexOfChar = "Kotlin".indexOf('t') //>indexOfChar     :2
+    val indexOfWord = "Kotlin best english".indexOf("best") //>indexOfWord     :7
+    val backReverse = "nitlok".reversed()
+    println(">repeat          :$repeat")
+    println(">letter          :$letter")
+    println(">indexOfChar     :$indexOfChar")
+    println(">indexOfWord     :$indexOfWord")
+    println(">backReverse     :$backReverse")
+
+    val multilineString = """
+           Первая строка
+               Вторая строка
+         Третья строка
+    """.trimIndent()
+    println(">multilineString :$multilineString")
+
+
+    val string2 = "Sweet summer child"
+    val subStr1 = string2.substring(6) // включительно
+    val subStr2 = string2.substring(6, 12)  // вкл - не вкл
+    println()
+    println(string2)
+    println(">subStr1 :$subStr1")
+    println(">subStr2 :$subStr2")
+
+
+    //Форматирование строк
+    println("\n\n ============ Форматирование строк")
+    val nick = "Алексей"
+    val city = "Moscow"
+    val ag = 32
+    val friendsCount = 1052
+    val rating = 4.948
+    val balance = 234.75856
+    val text = """
+        Имя: %s
+        Город: %s
+        Возраст: %d
+        Кол-во друзей: %,d
+        Рейтинг: %.1f
+        Баланс: $%,.2f
+    """.trimIndent()
+
+// %s строка
+// %d число диджит
+// %f число флоат
+// %, запятая -> разбить на тысячи
+// %. выведет запятую
+
+// %,d c пробелом (разбить на тысячи)
+// %.1f  [точка + 1 -> запятая] + [колво знаков после запятой]
+// $%,.2f [разбить на тысячи] + [точка+2 -> запятая + колво знаков]
+
+    println(text.format(nick, city, ag, friendsCount, rating, balance))
+
+    println(">>> TXT: %s".format("Kotlin")) // >>> TXT: Kotlin
+    println(">>> TXT: %10s".format("Kotlin")) //>>> TXT:     Kotlin
+    println(">>> TXT: %-10s".format("Kotlin")) //>>> TXT: Kotlin
+
+    println(">>> quantity: %10d".format(11)) //>>> quantity: 3
+    println(">>> quantity: %-10d".format(11)) //>>> quantity: 3
+
+    println(">>> PRICE: %.2f".format(3.14159)) //>>> PRICE: 3,14
+    println(">>> PRICE: %6.2f".format(3.14159)) //>>> PRICE:   3,14
+    println(">>> PRICE decimal: %,d".format(1000000)) //>>> PRICE decimal: 1 000 000
+
+}
+
+fun getDetails(): String {
+    return "Очень интересные детали"
+}
+
+class Person(val name: String, val age: Int)
+
+class Lesson8Lection {
+}
